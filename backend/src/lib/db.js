900fcb4d1454e5153bd195a63965816f4f963fae
Sanketagrawal08 <sanketagrawal6969@gmail.com>
2025-04-12 00:05:42 +0530

    D\

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,6 +5,6 @@ export const connectDB = async () => {
     const conn = await mongoose.connect("mongodb+srv://sanketagrawal6969:3HQkaPT8fBOSvfGa@cluster0.3hign.mongodb.net/chatdb?retryWrites=true&w=majority&appName=Cluster0");
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log("MongoDB connection error:", error);
+    console.log("MongoDB connection error in databse:", error);
   }
 };
