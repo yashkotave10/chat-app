2aa04249f91b746b1f68de57f307c6d71e903a5a
Sanketagrawal08 <sanketagrawal6969@gmail.com>
2025-04-12 14:08:02 +0530

    sanket

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "Strict", 
+    sameSite: "none", 
     secure: true,
   });
 
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,8 @@ export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5001/api",
   withCredentials: true,
 });
+axios.defaults.withCredentials = true;
+
 
 //for local testign
 // export const axiosInstance = axios.create({
